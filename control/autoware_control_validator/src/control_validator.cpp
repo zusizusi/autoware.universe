79cc06fe67910a0207674631aaa8f5782c912a0a
Yuki TAKAGI <141538661+yuki-takagi-66@users.noreply.github.com>
2025-04-21 21:34:50 +0900

    fix(control_validator): fix less trajectory point check (#10508)
    Signed-off-by: yuki-takagi-66 <yuki.takagi@tier4.jp>

--- control/autoware_control_validator/src/control_validator.cpp
+++ control/autoware_control_validator/src/control_validator.cpp
@@ -102,21 +102,11 @@ void OverrunValidator::validate(
   ControlValidatorStatus & res, const Trajectory & reference_trajectory,
   const Odometry & kinematics)
 {
-  res.dist_to_stop = [](const Trajectory & traj, const geometry_msgs::msg::Pose & pose) {
-    const auto stop_idx_opt = autoware::motion_utils::searchZeroVelocityIndex(traj.points);
-
-    const size_t end_idx = stop_idx_opt ? *stop_idx_opt : traj.points.size() - 1;
-    const size_t seg_idx =
-      autoware::motion_utils::findFirstNearestSegmentIndexWithSoftConstraints(traj.points, pose);
-    const double signed_length_on_traj = autoware::motion_utils::calcSignedArcLength(
-      traj.points, pose.position, seg_idx, traj.points.at(end_idx).pose.position,
-      std::min(end_idx, traj.points.size() - 2));
-
-    if (std::isnan(signed_length_on_traj)) {
-      return 0.0;
-    }
-    return signed_length_on_traj;
-  }(reference_trajectory, kinematics.pose.pose);
+  const auto stop_idx_opt =
+    autoware::motion_utils::searchZeroVelocityIndex(reference_trajectory.points);
+  const size_t end_idx = stop_idx_opt ? *stop_idx_opt : reference_trajectory.points.size() - 1;
+  res.dist_to_stop = autoware::motion_utils::calcSignedArcLength(
+    reference_trajectory.points, kinematics.pose.pose.position, end_idx);
 
   res.nearest_trajectory_vel =
     autoware::motion_utils::calcInterpolatedPoint(reference_trajectory, kinematics.pose.pose)
@@ -273,15 +263,13 @@ void ControlValidator::on_control_cmd(const Control::ConstSharedPtr msg)
   if (!predicted_trajectory_msg) {
     return waiting(sub_reference_traj_->subscriber()->get_topic_name());
   }
-  if (predicted_trajectory_msg->points.size() < 2) {
-    RCLCPP_DEBUG(get_logger(), "predicted_trajectory size is less than 2. Cannot validate.");
-    return;
-  }
   Trajectory::ConstSharedPtr reference_trajectory_msg = sub_reference_traj_->take_data();
   if (!reference_trajectory_msg) {
     return waiting(sub_reference_traj_->subscriber()->get_topic_name());
   }
   if (reference_trajectory_msg->points.size() < 2) {
+    // TODO(takagi): This check should be moved into each of the individual validate() functions.
+    // Passing the rclcpp::Logger as an argument to the validate() function is necessary.
     RCLCPP_ERROR_THROTTLE(
       get_logger(), *get_clock(), 1000,
       "reference_trajectory size is less than 2. Cannot validate.");
@@ -302,8 +290,14 @@ void ControlValidator::on_control_cmd(const Control::ConstSharedPtr msg)
 
   // validation process
   latency_validator.validate(validation_status_, *control_cmd_msg, *this);
-  trajectory_validator.validate(
-    validation_status_, *predicted_trajectory_msg, *reference_trajectory_msg);
+  if (predicted_trajectory_msg->points.size() < 2) {
+    // TODO(takagi): This check should be moved into each of the individual validate() functions.
+    // Passing the rclcpp::Logger as an argument to the validate() function is necessary.
+    RCLCPP_DEBUG(get_logger(), "predicted_trajectory size is less than 2. Cannot validate.");
+  } else {
+    trajectory_validator.validate(
+      validation_status_, *predicted_trajectory_msg, *reference_trajectory_msg);
+  }
   acceleration_validator.validate(
     validation_status_, *kinematics_msg, *control_cmd_msg, *acceleration_msg);
   velocity_validator.validate(validation_status_, *reference_trajectory_msg, *kinematics_msg);
