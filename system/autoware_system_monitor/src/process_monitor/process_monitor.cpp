5836d1708a4237afcf2d9c7e02f94e813c3461cd
TaikiYamada4 <129915538+TaikiYamada4@users.noreply.github.com>
2025-04-22 11:30:31 +0900

    fix(autoware_system_monitor): quick fix by cherry-picking 677447f (#1998)
    fix(autoware_system_monitor): quick fix for autoware_system_monitor (#10506)
    
    * feat(autoware_system_monitor): quick fix, autoware_system_monitor, fix sompile issue : v0.0
    
    
    
    * feat(autoware_system_monitor): quick fix, autoware_system_monitor, add comment by sasaki san: v0.1
    
    
    
    ---------
    
    Signed-off-by: liuXinGangChina <lxg19892021@gmail.com>
    Co-authored-by: 心刚 <90366790+liuXinGangChina@users.noreply.github.com>

--- system/autoware_system_monitor/src/process_monitor/process_monitor.cpp
+++ system/autoware_system_monitor/src/process_monitor/process_monitor.cpp
@@ -567,7 +567,11 @@ bool ProcessMonitor::getCommandLineFromPid(const std::string & pid, std::string 
   // Leave the last 0x00 (end-of-C-string) intact.
   std::replace(buffer.begin(), (buffer.end() - 1), '\0', ' ');
   // Make sure the last character is a null terminator.
-  *(buffer.end() - 1) = '\0';
+
+  // Although buffer.back() is safer and more idiomatic,
+  // using it here causes a compile error with -Werror=stringop-overflow.
+  // Therefore, we safely access the last element by index.
+  buffer[buffer.size() - 1] = '\0';
   try {
     std::string cmdline = std::string(buffer.begin(), (buffer.end() - 1));
     // Remove trailing spaces
