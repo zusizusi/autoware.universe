a6d5003d062d41f3cc4aa23374de435c5400f6cc
Kenzo Lobos Tsunekawa <kenzo.lobos@tier4.jp>
2025-04-22 15:33:08 +0900

    feat(autoware_cuda_pointcloud_preprocessor): pointcloud concatenation (#10300)
    * feat: cuda accelerated version of the pointcloud concatenation
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: removed duplicated include
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: changed to header blocks from pragmas :c
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: removed yaml and schema since this node uses the same interface as the non-gpu node
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: fixed rebased induced error
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * fix: used the wrong point type
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: changed pointer to auto
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: rewrote equation for clarity
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: added a comment regarding the reallocation strategy
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: reflected latest changes in the templated version of the concat
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: addressed cppcheck reports
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: fixed dead link
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: solving uncrustify conflicts
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: more uncrustify
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: yet another uncrustify related error
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: hopefully last uncrustify error
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * chore: now fixing uncrustify on source files
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    ---------
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>

---
+++ sensing/autoware_cuda_pointcloud_preprocessor/include/autoware/cuda_pointcloud_preprocessor/cuda_concatenate_data/cuda_concatenate_and_time_sync_node.hpp
@@ -0,0 +1,47 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* *INDENT-OFF* */
+#ifndef AUTOWARE__CUDA_POINTCLOUD_PREPROCESSOR__CUDA_CONCATENATE_DATA__CUDA_CONCATENATE_AND_TIME_SYNC_NODE_HPP_
+#define AUTOWARE__CUDA_POINTCLOUD_PREPROCESSOR__CUDA_CONCATENATE_DATA__CUDA_CONCATENATE_AND_TIME_SYNC_NODE_HPP_
+
+#include "autoware/cuda_pointcloud_preprocessor/cuda_concatenate_data/cuda_traits.hpp"
+#include "autoware/pointcloud_preprocessor/concatenate_data/concatenate_and_time_sync_node.hpp"
+
+#include <cuda_blackboard/cuda_blackboard_publisher.hpp>
+#include <cuda_blackboard/cuda_blackboard_subscriber.hpp>
+#include <cuda_blackboard/cuda_pointcloud2.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+namespace autoware::cuda_pointcloud_preprocessor
+{
+
+class CudaPointCloudConcatenateDataSynchronizerComponent
+: public autoware::pointcloud_preprocessor::PointCloudConcatenateDataSynchronizerComponentTemplated<
+    autoware::pointcloud_preprocessor::CudaPointCloud2Traits>
+{
+public:
+  explicit CudaPointCloudConcatenateDataSynchronizerComponent(
+    const rclcpp::NodeOptions & node_options)
+  : autoware::pointcloud_preprocessor::PointCloudConcatenateDataSynchronizerComponentTemplated<
+      autoware::pointcloud_preprocessor::CudaPointCloud2Traits>(node_options)
+  {
+  }
+  ~CudaPointCloudConcatenateDataSynchronizerComponent() override = default;
+};
+
+}  // namespace autoware::cuda_pointcloud_preprocessor
+
+#endif  // AUTOWARE__CUDA_POINTCLOUD_PREPROCESSOR__CUDA_CONCATENATE_DATA__CUDA_CONCATENATE_AND_TIME_SYNC_NODE_HPP_
+/* *INDENT-ON* */
