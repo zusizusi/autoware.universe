e92e31c6ca0fc3d5e502ea04aeb1282f88c001d7
Masato Saeki <78376491+MasatoSaeki@users.noreply.github.com>
2025-05-14 20:44:12 +0900

    fix(traffic_light_utils): remove condition in `setSignalUnknown` (#10631)
    remove condition
    
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>

--- common/autoware_traffic_light_utils/src/traffic_light_utils.cpp
+++ common/autoware_traffic_light_utils/src/traffic_light_utils.cpp
@@ -24,10 +24,7 @@ void setSignalUnknown(tier4_perception_msgs::msg::TrafficLight & signal, float c
   signal.elements.resize(1);
   signal.elements[0].shape = tier4_perception_msgs::msg::TrafficLightElement::UNKNOWN;
   signal.elements[0].color = tier4_perception_msgs::msg::TrafficLightElement::UNKNOWN;
-  // the default value is -1, which means to not set confidence
-  if (confidence > 0) {
-    signal.elements[0].confidence = confidence;
-  }
+  signal.elements[0].confidence = confidence;
 }
 
 bool hasTrafficLightCircleColor(
