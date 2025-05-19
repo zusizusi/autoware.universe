d1cbe8e37f4cd566c49bba199f925580b0919226
Arjun Jagdish Ram <arjun.ram@tier4.jp>
2025-05-19 19:01:30 +0900

    fix(autoware_motion_velocity_obstacle_stop_module): fix for mishandling lateral-distance  (#2030)
    fix(autoware_motion_velocity_obstacle_slow_down_module): fix for mishandling lateral-distance (#10559)
    
    * Fix for mishandling lateral-distance
    
    
    
    ---------
    
    Signed-off-by: Arjun Jagdish Ram <arjun.ram@tier4.jp>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Taiki Yamada <129915538+TaikiYamada4@users.noreply.github.com>

--- planning/motion_velocity_planner/autoware_motion_velocity_obstacle_slow_down_module/src/obstacle_slow_down_module.hpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_obstacle_slow_down_module/src/obstacle_slow_down_module.hpp
@@ -90,19 +90,20 @@ private:
   std::vector<autoware::motion_velocity_planner::SlowDownPointData>
   convert_point_cloud_to_slow_down_points(
     const PlannerData::Pointcloud & pointcloud, const std::vector<TrajectoryPoint> & traj_points,
+    const std::vector<Polygon2d> & decimated_traj_polys_with_lat_margin,
     const VehicleInfo & vehicle_info, const size_t ego_idx);
   std::vector<SlowDownObstacle> filter_slow_down_obstacle_for_predicted_object(
     const Odometry & odometry, const double ego_nearest_dist_threshold,
-    const double ego_nearest_yaw_threshold, const std::vector<TrajectoryPoint> & traj_points,
-    const std::vector<TrajectoryPoint> & decimated_traj_points,
+    const double ego_nearest_yaw_threshold,
+    const std::vector<Polygon2d> & decimated_traj_polys_with_lat_margin,
+    const std::vector<TrajectoryPoint> & traj_points,
     const std::vector<std::shared_ptr<PlannerData::Object>> & objects,
     const rclcpp::Time & predicted_objects_stamp, const VehicleInfo & vehicle_info,
     const TrajectoryPolygonCollisionCheck & trajectory_polygon_collision_check);
   std::vector<SlowDownObstacle> filter_slow_down_obstacle_for_point_cloud(
-    const Odometry & odometry, const std::vector<TrajectoryPoint> & traj_points,
-    const std::vector<TrajectoryPoint> & decimated_traj_points,
-    const PlannerData::Pointcloud & point_cloud, const VehicleInfo & vehicle_info,
-    const TrajectoryPolygonCollisionCheck & trajectory_polygon_collision_check, size_t ego_idx);
+    const std::vector<TrajectoryPoint> & traj_points,
+    const std::vector<Polygon2d> & decimated_traj_polys_with_lat_margin,
+    const PlannerData::Pointcloud & point_cloud, const VehicleInfo & vehicle_info, size_t ego_idx);
   std::optional<SlowDownObstacle> create_slow_down_obstacle_for_predicted_object(
     const std::vector<TrajectoryPoint> & traj_points,
     const std::vector<Polygon2d> & decimated_traj_polys_with_lat_margin,
