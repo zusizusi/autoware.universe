4a891217ff5b367c1418e6d992a1d92a009d329f
Yuxuan Liu <619684051@qq.com>
2025-07-03 19:00:06 +0900

    feat(control_validator): disable control validator error report when not in autonomous control (#10871)
    * feat(control_validator): disable control validator error report when not in autonomous control
    
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>
    
    * also fix control validator launch
    
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>
    
    * fix: work with yaw validator; fix yaw validator missing in all valid; add a debug info
    
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>
    
    * chore: get rid of the yaw warn in is_all_valid
    
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>
    
    * chore: fix topic remapping in launch file
    
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>
    
    ---------
    
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>

--- control/autoware_control_validator/include/autoware/control_validator/control_validator.hpp
+++ control/autoware_control_validator/include/autoware/control_validator/control_validator.hpp
@@ -26,6 +26,7 @@
 #include <autoware_utils/system/stop_watch.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+#include <autoware_adapi_v1_msgs/msg/operation_mode_state.hpp>
 #include <autoware_control_msgs/msg/control.hpp>
 #include <autoware_internal_debug_msgs/msg/float64_stamped.hpp>
 #include <autoware_planning_msgs/msg/trajectory.hpp>
@@ -41,6 +42,7 @@
 
 namespace autoware::control_validator
 {
+using autoware_adapi_v1_msgs::msg::OperationModeState;
 using autoware_control_msgs::msg::Control;
 using autoware_control_validator::msg::ControlValidatorStatus;
 using autoware_planning_msgs::msg::Trajectory;
@@ -275,8 +277,20 @@ private:
   void set_status(
     DiagnosticStatusWrapper & stat, const bool & is_ok, const std::string & msg) const;
 
+  /**
+   * @brief Infer autonomous control state
+   */
+  bool infer_autonomous_control_state(const OperationModeState::ConstSharedPtr);
+
+  /**
+   * @brief Postprocessing while keeping debug values
+   */
+  void validation_filtering(ControlValidatorStatus & res);
+
   // Subscribers and publishers
   rclcpp::Subscription<Control>::SharedPtr sub_control_cmd_;
+  autoware_utils::InterProcessPollingSubscriber<OperationModeState>::SharedPtr
+    sub_operational_state_;
   autoware_utils::InterProcessPollingSubscriber<Odometry>::SharedPtr sub_kinematics_;
   autoware_utils::InterProcessPollingSubscriber<Trajectory>::SharedPtr sub_reference_traj_;
   autoware_utils::InterProcessPollingSubscriber<Trajectory>::SharedPtr sub_predicted_traj_;
@@ -293,6 +307,7 @@ private:
   Updater diag_updater_{this};
   ControlValidatorStatus validation_status_;
   vehicle_info_utils::VehicleInfo vehicle_info_;
+  bool flag_autonomous_control_enabled_ = false;
   /**
    * @brief Check if all validation criteria are met
    * @param status Validation status
--- control/autoware_control_validator/src/control_validator.cpp
+++ control/autoware_control_validator/src/control_validator.cpp
@@ -206,6 +206,9 @@ ControlValidator::ControlValidator(const rclcpp::NodeOptions & options)
 
   sub_control_cmd_ = create_subscription<Control>(
     "~/input/control_cmd", 1, std::bind(&ControlValidator::on_control_cmd, this, _1));
+  sub_operational_state_ =
+    autoware_utils::InterProcessPollingSubscriber<OperationModeState>::create_subscription(
+      this, "~/input/operational_mode_state", 1);
   sub_kinematics_ =
     autoware_utils::InterProcessPollingSubscriber<nav_msgs::msg::Odometry>::create_subscription(
       this, "~/input/kinematics", 1);
@@ -325,6 +328,26 @@ void ControlValidator::setup_diag()
   });
 }
 
+bool ControlValidator::infer_autonomous_control_state(const OperationModeState::ConstSharedPtr msg)
+{
+  return (msg->mode == OperationModeState::AUTONOMOUS) && (msg->is_autoware_control_enabled);
+}
+
+void ControlValidator::validation_filtering(ControlValidatorStatus & res)
+{
+  // Set all boolean status into valid state
+  res.is_valid_max_distance_deviation = true;
+  res.is_valid_acc = true;
+  res.is_rolling_back = false;
+  res.is_over_velocity = false;
+  res.is_valid_lateral_jerk = true;
+  res.has_overrun_stop_point = false;
+  res.will_overrun_stop_point = false;
+  res.is_valid_latency = true;
+  res.is_valid_yaw = true;
+  res.is_warn_yaw = false;
+}
+
 void ControlValidator::on_control_cmd(const Control::ConstSharedPtr msg)
 {
   stop_watch.tic();
@@ -354,6 +377,10 @@ void ControlValidator::on_control_cmd(const Control::ConstSharedPtr msg)
       get_logger(), *get_clock(), 1000,
       "reference_trajectory size is less than 2. Cannot validate.");
     return;
+  }
+  OperationModeState::ConstSharedPtr operation_mode_msg = sub_operational_state_->take_data();
+  if (operation_mode_msg) {
+    flag_autonomous_control_enabled_ = infer_autonomous_control_state(operation_mode_msg);
   }
   Odometry::ConstSharedPtr kinematics_msg = sub_kinematics_->take_data();
   if (!kinematics_msg) {
@@ -388,9 +415,19 @@ void ControlValidator::on_control_cmd(const Control::ConstSharedPtr msg)
   overrun_validator.validate(validation_status_, *reference_trajectory_msg, *kinematics_msg);
   yaw_validator.validate(validation_status_, *reference_trajectory_msg, *kinematics_msg);
 
+  if (!flag_autonomous_control_enabled_) {
+    // if warnings or errors are being suppressed, printing simple logs
+    if (!is_all_valid(validation_status_)) {
+      RCLCPP_DEBUG_THROTTLE(
+        get_logger(), *get_clock(), 3000, "Suppressing control validation during manual driving");
+    }
+    validation_filtering(validation_status_);
+  }
+
   // post process
   validation_status_.invalid_count =
     is_all_valid(validation_status_) ? 0 : validation_status_.invalid_count + 1;
+
   diag_updater_.force_update();
 
   publish_debug_info(kinematics_msg->pose.pose);
@@ -420,7 +457,7 @@ bool ControlValidator::is_all_valid(const ControlValidatorStatus & s)
 {
   return s.is_valid_lateral_jerk && s.is_valid_max_distance_deviation && s.is_valid_acc &&
          !s.is_rolling_back && !s.is_over_velocity && !s.has_overrun_stop_point &&
-         !s.will_overrun_stop_point;
+         !s.will_overrun_stop_point && s.is_valid_yaw;
 }
 
 std::string ControlValidator::generate_error_message(const ControlValidatorStatus & s)
