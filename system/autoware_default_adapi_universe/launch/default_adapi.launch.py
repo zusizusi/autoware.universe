4d0d0ec40afb827eb87d0a3ca23e4760ced6bbc4
Ryohsuke Mitsudome <43976834+mitsudome-r@users.noreply.github.com>
2025-07-07 18:34:15 +0900

    feat(autoware_default_adapi_universe)!: remove interface ported to Autoware Core (#10918)
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- system/autoware_default_adapi_universe/launch/default_adapi.launch.py
+++ system/autoware_default_adapi_universe/launch/default_adapi.launch.py
@@ -24,12 +24,12 @@ from launch_ros.parameter_descriptions import ParameterFile
 from launch_ros.substitutions import FindPackageShare
 
 
-def create_api_node(node_name, class_name):
+def create_api_node(package_name, node_name, class_name):
     fullname = pathlib.Path("adapi/node") / node_name
     return ComposableNode(
         namespace=str(fullname.parent),
         name=str(fullname.name),
-        package="autoware_default_adapi_universe",
+        package=package_name,
         plugin="autoware::default_adapi::" + class_name,
         parameters=[ParameterFile(LaunchConfiguration("config"))],
     )
@@ -43,25 +43,25 @@ def get_default_config():
 
 def generate_launch_description():
     components = [
-        create_api_node("autoware_state", "AutowareStateNode"),
-        create_api_node("diagnostics", "DiagnosticsNode"),
-        create_api_node("fail_safe", "FailSafeNode"),
-        create_api_node("heartbeat", "HeartbeatNode"),
-        create_api_node("interface", "InterfaceNode"),
-        create_api_node("localization", "LocalizationNode"),
-        create_api_node("manual/local", "ManualControlNode"),
-        create_api_node("manual/remote", "ManualControlNode"),
-        create_api_node("motion", "MotionNode"),
-        create_api_node("mrm_request", "MrmRequestNode"),
-        create_api_node("operation_mode", "OperationModeNode"),
-        create_api_node("perception", "PerceptionNode"),
-        create_api_node("planning", "PlanningNode"),
-        create_api_node("routing", "RoutingNode"),
-        create_api_node("vehicle_status", "VehicleStatusNode"),
-        create_api_node("vehicle_command", "VehicleCommandNode"),
-        create_api_node("vehicle_metrics", "VehicleMetricsNode"),
-        create_api_node("vehicle_info", "VehicleInfoNode"),
-        create_api_node("vehicle_door", "VehicleDoorNode"),
+        create_api_node("autoware_default_adapi", "interface", "InterfaceNode"),
+        create_api_node("autoware_default_adapi", "localization", "LocalizationNode"),
+        create_api_node("autoware_default_adapi", "routing", "RoutingNode"),
+        create_api_node("autoware_default_adapi_universe", "autoware_state", "AutowareStateNode"),
+        create_api_node("autoware_default_adapi_universe", "diagnostics", "DiagnosticsNode"),
+        create_api_node("autoware_default_adapi_universe", "fail_safe", "FailSafeNode"),
+        create_api_node("autoware_default_adapi_universe", "heartbeat", "HeartbeatNode"),
+        create_api_node("autoware_default_adapi_universe", "manual/local", "ManualControlNode"),
+        create_api_node("autoware_default_adapi_universe", "manual/remote", "ManualControlNode"),
+        create_api_node("autoware_default_adapi_universe", "motion", "MotionNode"),
+        create_api_node("autoware_default_adapi_universe", "mrm_request", "MrmRequestNode"),
+        create_api_node("autoware_default_adapi_universe", "operation_mode", "OperationModeNode"),
+        create_api_node("autoware_default_adapi_universe", "perception", "PerceptionNode"),
+        create_api_node("autoware_default_adapi_universe", "planning", "PlanningNode"),
+        create_api_node("autoware_default_adapi_universe", "vehicle_status", "VehicleStatusNode"),
+        create_api_node("autoware_default_adapi_universe", "vehicle_command", "VehicleCommandNode"),
+        create_api_node("autoware_default_adapi_universe", "vehicle_metrics", "VehicleMetricsNode"),
+        create_api_node("autoware_default_adapi_universe", "vehicle_info", "VehicleInfoNode"),
+        create_api_node("autoware_default_adapi_universe", "vehicle_door", "VehicleDoorNode"),
     ]
     container = ComposableNodeContainer(
         namespace="adapi",
