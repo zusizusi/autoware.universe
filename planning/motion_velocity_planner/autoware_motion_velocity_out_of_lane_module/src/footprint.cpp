b5921251c5eeddf15b9507eca2e2b059c1abbbbe
Maxime CLEMENT <78338830+maxime-clem@users.noreply.github.com>
2025-07-11 16:56:36 +0900

    feat(out_of_lane): cut trajectory footprints behind the current ego front (#10949)
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/motion_velocity_planner/autoware_motion_velocity_out_of_lane_module/src/footprint.cpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_out_of_lane_module/src/footprint.cpp
@@ -15,13 +15,18 @@
 #include "footprint.hpp"
 
 #include <autoware_utils/geometry/boost_polygon_utils.hpp>
+#include <autoware_utils_geometry/boost_geometry.hpp>
+#include <autoware_utils_geometry/geometry.hpp>
+#include <autoware_utils_math/normalization.hpp>
+#include <tf2/utils.hpp>
 
+#include <autoware_planning_msgs/msg/detail/trajectory_point__struct.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 
 #include <boost/geometry/algorithms/correct.hpp>
+#include <boost/geometry/algorithms/detail/intersects/interface.hpp>
 
 #include <lanelet2_core/geometry/Polygon.h>
-#include <tf2/utils.h>
 
 #include <vector>
 
@@ -43,32 +48,48 @@ autoware_utils::Polygon2d make_base_footprint(const PlannerParam & p, const bool
   return base_footprint;
 }
 
-lanelet::BasicPolygon2d project_to_pose(
-  const autoware_utils::Polygon2d & base_footprint, const geometry_msgs::msg::Pose & pose)
+lanelet::BasicPolygon2d project_to_trajectory_point(
+  const autoware_utils::Polygon2d & footprint,
+  const autoware_planning_msgs::msg::TrajectoryPoint & trajectory_point)
 {
-  const auto angle = tf2::getYaw(pose.orientation);
-  const auto rotated_footprint = autoware_utils::rotate_polygon(base_footprint, angle);
-  lanelet::BasicPolygon2d footprint;
-  for (const auto & p : rotated_footprint.outer())
-    footprint.emplace_back(p.x() + pose.position.x, p.y() + pose.position.y);
-  return footprint;
+  lanelet::BasicPolygon2d projected_footprint;
+  const auto angle = tf2::getYaw(trajectory_point.pose.orientation);
+  const auto rotated_footprint = autoware_utils::rotate_polygon(footprint, angle);
+  for (const auto & p : rotated_footprint.outer()) {
+    projected_footprint.emplace_back(
+      p.x() + trajectory_point.pose.position.x, p.y() + trajectory_point.pose.position.y);
+  }
+  return projected_footprint;
 }
 
 std::vector<lanelet::BasicPolygon2d> calculate_trajectory_footprints(
   const EgoData & ego_data, const PlannerParam & params)
 {
-  const auto base_footprint = make_base_footprint(params);
   std::vector<lanelet::BasicPolygon2d> trajectory_footprints;
   trajectory_footprints.reserve(ego_data.trajectory_points.size());
+  const auto base_footprint = make_base_footprint(params);
+  // create a polygon to cut the trajectory footprints behind the current front of the ego vehicle
+  constexpr auto cut_multiplier = 10.0;
+  const auto cut_polygon = autoware_utils_geometry::to_footprint(
+    ego_data.pose, params.front_offset, -params.rear_offset * cut_multiplier,
+    params.left_offset * cut_multiplier);
+  auto arc_length = 0.0;
   for (auto i = 0UL; i < ego_data.trajectory_points.size(); ++i) {
-    const auto & trajectory_pose = ego_data.trajectory_points[i].pose;
-    const auto angle = tf2::getYaw(trajectory_pose.orientation);
-    const auto rotated_footprint = autoware_utils::rotate_polygon(base_footprint, angle);
-    lanelet::BasicPolygon2d footprint;
-    for (const auto & p : rotated_footprint.outer())
-      footprint.emplace_back(
-        p.x() + trajectory_pose.position.x, p.y() + trajectory_pose.position.y);
-    trajectory_footprints.push_back(footprint);
+    const auto & trajectory_point = ego_data.trajectory_points[i];
+    const auto trajectory_footprint = project_to_trajectory_point(base_footprint, trajectory_point);
+    // we only apply the cut to the beginning of the trajectory
+    if (arc_length > params.front_offset) {
+      trajectory_footprints.push_back(trajectory_footprint);
+      continue;
+    }
+    if (i + 1 < ego_data.trajectory_points.size()) {
+      arc_length +=
+        autoware_utils::calc_distance2d(trajectory_point, ego_data.trajectory_points[i + 1]);
+    }
+    lanelet::BasicPolygons2d cut_result;
+    boost::geometry::difference(trajectory_footprint, cut_polygon, cut_result);
+    trajectory_footprints.push_back(
+      cut_result.empty() ? lanelet::BasicPolygon2d() : cut_result.front());
   }
   return trajectory_footprints;
 }
